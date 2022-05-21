c27cfdcb9351aa3773022165b7847009e603adb0
lam <shun_min@hotmail.com>
2022-05-21 22:32:37 +0800

    - update UI

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
+import os
 import sys
+
 import m3u8_To_MP4
 from PySide2 import QtWidgets, QtGui, QtCore
 
@@ -8,27 +10,40 @@ class Downloader(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
+        self.set_connections()
 
     def init_ui(self):
-        self.label = QtWidgets.QLabel("Paste path to download")
-        self.layout = QtWidgets.QHBoxLayout()
+        self.vlayout = QtWidgets.QVBoxLayout()
+        self.instr_label = QtWidgets.QLabel("Paste url to download")
         self.link_txt = QtWidgets.QLineEdit()
+        self.path_label = QtWidgets.QLabel("Download to directory")
+
+        self.hlayout = QtWidgets.QHBoxLayout()
+        self.dir_btn = QtWidgets.QPushButton("Select...")
         self.downlaod_path_txt = QtWidgets.QLineEdit()
         self.download_btn = QtWidgets.QPushButton("Download")
 
-        self.layout.addWidget(self.label)
-        self.layout.addWidget(self.link_txt)
-        self.layout.addWidget(self.downlaod_path_txt)
-        self.layout.addWidget(self.download_btn)
+        self.hlayout.addWidget(self.downlaod_path_txt)
+        self.hlayout.addWidget(self.dir_btn)
 
-        self.setLayout(self.layout)
+        self.vlayout.addWidget(self.instr_label)
+        self.vlayout.addWidget(self.link_txt)
+        self.vlayout.addLayout(self.hlayout)
+        self.vlayout.addWidget(self.download_btn)
+
+        self.setLayout(self.vlayout)
 
     def set_connections(self):
+        self.dir_btn.clicked.connect(self.open_explorer)
         self.download_btn.clicked.connect(self.download)
 
+    def open_explorer(self):
+        os.system("xdg-open '%s'" % "/home")
+
     def download(self):
-        # https://v.redd.it/gi4n5i6qeyt81/HLSPlaylist.m3u8
+        # https://v.redd.it/gi4n5i6qeyt81/HLSPlaylist.m3u8wecha
         #/home/lam/Documents/PycharmProjects/m3u8Download/sample
+        print("<<<<<<{} {}".format(self.link_txt.text(), self.downlaod_path_txt.text()))
         m3u8_To_MP4.download(self.link_txt.text(), mp4_file_dir=self.downlaod_path_txt.text())
 
 if __name__ == "__main__":
