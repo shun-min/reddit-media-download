43f087dec9bbc7bc8e38f073e52586101a4e0b43
randomSeed <shun_min@hotmail.com>
2022-12-22 23:36:22 +0800

    use reddit API

--- src/linkScrapper.py
+++ src/linkScrapper.py
@@ -1,8 +1,16 @@
 #!/usr/lib64/python3.6
-
-import re
+import requests
+import requests.auth
 from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup
+
+from utils import REDDIT
+
+id = REDDIT.ID
+secret = REDDIT.SECRET
+
+requests.auth.HTTPBasicAuth(id, secret)
+post_data = 
+
 
 url = "https://www.reddit.com/r/malaysia/comments/vcnhkc/daylight_supermoon_near_the_tip_of_kl_twin_towers/"
 user_agent = {'User-agent': 'Mozilla/5.0'}
@@ -21,3 +29,10 @@ return results
 
 # for full_url in results:
     print(full_url)
+
+
+from bs4 import BeautifulSoup
+
+url = "https://www.reddit.com/r/malaysia/comments/vcnhkc/daylight_supermoon_near_the_tip_of_kl_twin_towers/"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
