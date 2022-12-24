182b505cc34e2b6d0430392272d1f5c76af659ef
xxrandomSeedxx <shun_min@hotmail.com>
2022-12-24 22:46:51 +0800

    - get video link using API

--- src/linkScrapper.py
+++ src/linkScrapper.py
@@ -20,30 +20,34 @@ headers = {
 # get token access ID
 TOKEN_ACCESS_ENDPOINT = "https://www.reddit.com/api/v1/access_token"
 response = requests.post(TOKEN_ACCESS_ENDPOINT, data=post_data, headers=headers, auth=client_auth)
-if response.reason == 200:
-    response.json()["access_token"]
+if response.status_code == 200:
+    token_id = response.json()["access_token"]
 
-# Failed attempt 1
-# url = "https://www.reddit.com/r/malaysia/comments/vcnhkc/daylight_supermoon_near_the_tip_of_kl_twin_towers/"
-# user_agent = {'User-agent': 'Mozilla/5.0'}
-# req = Request(url, headers=user_agent)
-# page = urlopen(req)
-# html_bytes = page.read()
-# html = html_bytes.decode("utf-8")
-#
-# # TODO: Test string for UI dev
-# # html = '123https://v.redd.it/0sljhvn5sm5a1/HLSPlaylist.m3u8123'
-# ext = "m3u8"
-# raw_pattern = r".+(?P<link>https.+.{}).+".format(ext)
-# pattern = re.compile(raw_pattern)
-# results = re.findall(pattern, html)
-#
-# # for full_url in results:
-#     print(full_url)
-#
-# Failed attempt 2
-# from bs4 import BeautifulSoup
-#
-# url = "https://www.reddit.com/r/malaysia/comments/vcnhkc/daylight_supermoon_near_the_tip_of_kl_twin_towers/"
-# page = requests.get(url)
-# soup = BeautifulSoup(page.content, "html.parser")
+# use API to perform operations
+OAUTH_ENDPOINT = "https://oauth.reddit.com"
+params = {
+    "limit": 100
+}
+header2 = {
+    "User-Agent": "Reddit automation script",
+    "Authorization": "Bearer" + token_id
+}
+
+response2 = requests.get(OAUTH_ENDPOINT + "/r/malaysia/comments/vcnhkc/daylight_supermoon_near_the_tip_of_kl_twin_towers/.json", headers=header2, params=params)
+if response2.status_code == 200:
+    data = response2.json()
+    children = data[0]["data"]["children"]
+
+for c in children:
+    try:
+        parent_list = c["data"]["crosspost_parent_list"]
+        break
+    except KeyError:
+        continue
+
+for p in parent_list:
+    try:
+        video_link = p["media"]["reddit_video"]["fallback_url"]
+        break
+    except KeyError:
+        continue
