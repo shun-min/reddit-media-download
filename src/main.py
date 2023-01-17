782545cb2fdaf47f87d4f4bfbaff05c86599a188
xxrandomSeedxx <shun_min@hotmail.com>
2023-01-17 23:07:11 +0800

    -update UI

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ import re
 import sys
 
 import m3u8_To_MP4
-from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 from utils import get_auth
 
-class Downloader(QtWidgets.QWidget):
+class Downloader(QWidget):
     def __init__(self):
         super().__init__()
         self.video_links = list()
@@ -15,28 +16,38 @@ class Downloader(QtWidgets.QWidget):
         self.set_connections()
 
     def init_ui(self):
-        self.setWindowTitle("m3u8 to mp4 Downloader")
-        self.setFixedWidth(500)
-        self.setFixedHeight(250)
+        self.setWindowTitle("Media Downloader")
+        self.setMinimumWidth(500)
+        self.setMinimumHeight(250)
 
-        self.vlayout = QtWidgets.QVBoxLayout()
-        self.src_layout = QtWidgets.QHBoxLayout()
-        self.src_label = QtWidgets.QLabel("Paste url to scrape:")
-        self.url_txt = QtWidgets.QLineEdit()
-        self.path_label = QtWidgets.QLabel("Download to directory")
-        self.dst_layout = QtWidgets.QHBoxLayout()
-        self.dst_label = QtWidgets.QLabel("Select destination folder")
-        self.dir_btn = QtWidgets.QPushButton("Select...")
-        self.get_media_btn = QtWidgets.QPushButton("Get Media from URL")
-        self.downlaod_path_txt = QtWidgets.QLineEdit()
-        self.download_btn = QtWidgets.QPushButton("Download")
+        self.vlayout = QVBoxLayout()
+        self.src_layout = QHBoxLayout()
+        self.src_label = QLabel("Paste url to scrape:")
+        self.url_txt = QLineEdit()
+        self.path_label = QLabel("Download to directory")
+        self.dst_layout = QHBoxLayout()
+        self.dst_label = QLabel("Select destination folder")
+        self.dir_btn = QPushButton("Select...")
+        self.get_media_btn = QPushButton("Get Media from URL")
+        self.downlaod_path_txt = QLineEdit()
+        self.download_btn = QPushButton("Download")
 
-        self.video_tree_list = QtWidgets.QTreeWidget()
-        self.img_tree_list = QtWidgets.QTreeWidget()
+        self.video_label = QLabel("Videos: ")
+        self.video_tree_list = QTreeView()
+        self.video_tree_list.setHeaderHidden(True)
+        self.image_label = QLabel("Images: ")
+        self.img_tree_list = QTreeView()
+        self.img_tree_list.setHeaderHidden(True)
 
-        self.media_layout = QtWidgets.QHBoxLayout()
-        self.media_layout.addWidget(self.video_tree_list)
-        self.media_layout.addWidget(self.img_tree_list)
+        self.media_layout = QHBoxLayout()
+        self.video_layout = QVBoxLayout()
+        self.image_layout = QVBoxLayout()
+        self.video_layout.addWidget(self.video_label)
+        self.video_layout.addWidget(self.video_tree_list)
+        self.image_layout.addWidget(self.image_label)
+        self.image_layout.addWidget(self.img_tree_list)
+        self.media_layout.addLayout(self.video_layout)
+        self.media_layout.addLayout(self.image_layout)
 
         self.src_layout.addWidget(self.src_label)
         self.src_layout.addWidget(self.url_txt)
@@ -59,13 +70,13 @@ class Downloader(QtWidgets.QWidget):
         self.get_media_btn.clicked.connect(self.get_media_links)
         self.download_btn.clicked.connect(self.download)
 
-
     def add_media_to_tree(self):
-        for media_item in self.video_links:
-            tree_item = QtWidgets.QTreeWidgetItem(self.video_tree_list)
-            tree_item.setText(0, media_item)
+        tree_item = QStringListModel()
+        tree_item.setStringList(self.video_links)
+        self.video_tree_list.setModel(tree_item)
 
     def get_media_links(self):
+        # https://reddit.com/r/malaysia/comments/vcnhkc/daylight_supermoon_near_the_tip_of_kl_twin_towers
         data = get_auth(self.url_txt.text())
         parent_lists = list()
         for d in data:
@@ -87,7 +98,7 @@ class Downloader(QtWidgets.QWidget):
         self.add_media_to_tree()
 
     def set_directory(self):
-        folder_path = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select..."))
+        folder_path = str(QFileDialog.getExistingDirectory(self, "Select..."))
         self.downlaod_path_txt.setText(folder_path)
 
     def download(self):
@@ -95,7 +106,7 @@ class Downloader(QtWidgets.QWidget):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QApplication([])
 
     downlaoder = Downloader()
     downlaoder.show()
